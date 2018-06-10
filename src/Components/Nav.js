80751208093ca1bfc2fde9fcb296aba57ca22422
tjallen27 <tjallen91@hotmail.co.uk>
2018-06-10 19:28:00 +0100

    Nav styling

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react';
+import logo from '../assets/logo.png';
 
 export default class Nav extends Component {
   render(){
     return(
-      <ul className="navList">
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+      <div>
+        <ul className="navList">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <img src={logo} className="logo" />
+      </div>
     )
   }
 }
